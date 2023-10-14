d37901950eddac81e1299b1f01551ef95f8006a5
JeanCDev <jeangames15@gmail.com>
2023-10-14 14:46:00 -0300

    Inclusão de termos

--- TermController.js
+++ TermController.js
@@ -1,31 +1,75 @@
-class TermController extends HtmlManipulatorController
+class TermController
 {
+  languages = {};
+  textFile = null;
+  termIndexes = [];
+  termConfig = null;
+
   constructor(termsConfig)
   {
-    super();
+    this.termConfig = termsConfig;
 
-    this.buildTable(termsConfig);
+    this.buildTable();
     this.createFileSaveEventListener();
   }
 
   /**
-   * Constrói a tabela de termos
-   * @param Object termsConfig
+   * Carrega os arquivos de termos
+   * @param string filename
    */
-  async buildTable(termsConfig)
+  async loadFiles(filename)
+  {
+    const file = await fetch(filename);
+    const json = await file.json();
+
+    return json;
+  }
+
+  /**
+   * Gera a url de download do arquivo modificado
+   * @param string text
+   */
+  async makeTextFile(text)
+  {
+    const filename = `${selectedLanguage.toLowerCase()}.json`
+    const file = await this.loadFiles(filename);
+
+    const newIndex = String(Number(Object.keys(file).slice(-1)) + 1);
+
+    if (Number(this.termIndexes.slice(-1)) <= Number(newIndex)) {
+      this.termIndexes.push(newIndex);
+    }
+
+    file[String(Number(Object.keys(file).slice(-1)) + 1)] = text;
+
+    const data = new Blob([JSON.stringify(file)], {type: 'application/json'});
+
+    if (this.textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    return window.URL.createObjectURL(data);
+  }
+
+  /**
+   * Constrói a tabela de termos
+   */
+  async buildTable()
   {
     const files = [];
     const headers = [];
 
-    for (const termConfig of termsConfig) {
+    for (const termConfig of this.termConfig) {
       const header = this.buildTableColumnsHeader(termConfig.language);
       headers.push(header);
 
       const file = await this.loadFiles(termConfig.file);
       files.push(file);
+
+      this.languages[termConfig.language] = file;
     }
 
-    this.createLine(headers, files, this.getIndexes(files[0]));
+    this.createLine(headers, files, this.getIndexes(files));
   }
 
   /**
@@ -56,8 +100,102 @@ class TermController extends HtmlManipulatorController
    * @param Object obj
    * @return Number[]
    */
-  getIndexes(obj)
+  getIndexes(languages)
   {
-    return Object.keys(obj);
+    for (let language of languages) {
+      const objectKeys = Object.keys(language);
+
+      if (objectKeys.length > this.termIndexes.length) {
+        this.termIndexes = objectKeys;
+      }
+    }
+
+    return this.termIndexes;
+  }
+
+  /**
+   * Cria uma linha da tabela de termos
+   * @param Array elements
+  */
+  buildTableLine(elements)
+  {
+    for (let element of elements) {
+      document.getElementById("terms").appendChild(this.buildTableColumns(element));
+    }
+  }
+
+  /**
+   * Cria o header da tabela de termos
+   * @param Array elements
+   */
+  buildTableHeader(elements)
+  {
+    for (let element of elements) {
+      document.getElementById("terms-header").appendChild(element);
+    }
+  }
+
+  /**
+   * Cria uma coluna na tabel de termos
+   * @param Object data
+   * @return HTMLTableRowElement
+   */
+  buildTableColumns(data)
+  {
+    let line = document.createElement('tr');
+    let indexCol = document.createElement('td');
+    indexCol.innerHTML = data.index;
+    line.appendChild(indexCol);
+
+    for (let term of data.terms) {
+      let column = document.createElement('td');
+
+      column.innerHTML = term || '';
+
+      line.appendChild(column);
+    }
+
+    return line;
+  }
+
+  /**
+   * Cria uma das colunas do header da tabela de termos
+   * @param String data
+   * @return HTMLTableCellElement
+   */
+  buildTableColumnsHeader(data)
+  {
+    let column = document.createElement('th');
+    column.innerHTML = `Termos ${data}`;
+
+    column.addEventListener('click', () => {
+      selectedLanguage = data;
+
+      document.getElementById('create').style.display = '';
+      document.getElementById('textbox').style.display = '';
+      document.getElementById('textbox').placeholder = `Inclua um novo termo para o idioma ${selectedLanguage}`;
+    });
+
+    return column;
+  }
+
+  /**
+   * Cria o evento que será executado ao clicar em salvar na tela de inclusão do termo
+   * @return void
+   */
+  async createFileSaveEventListener()
+  {
+    const create = document.getElementById('create');
+    const textBox = document.getElementById('textbox');
+
+    create.addEventListener('click', async() => {
+      const link = document.createElement('a');
+      const file = await this.makeTextFile(textBox.value);
+
+      link.setAttribute('download', `${selectedLanguage.toLowerCase()}.json`);
+      link.href = file;
+
+      link.click();
+    });
   }
 }
